7ba570812ff29b92be54799cbc755484e0b87085
Mansi-Tanwar <tanwarmansi211@gmail.com>
2025-09-18 14:44:50 +0530

    Updates

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: 'https://daybook-frontend.vercel.app', credentials: true }));
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
